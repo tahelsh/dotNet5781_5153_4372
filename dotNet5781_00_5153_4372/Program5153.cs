1c0c1b679aad37cebc1d34b12c057b95f79bf4dd
TahelSh <tahelsharon01@gmail.com>
2020-10-21 19:46:03 +0300

    intial code- Welcome5153

---
+++ dotNet5781_00_5153_4372/Program5153.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_5153_4372
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5153();
+            Welcome4372();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome4372();
+
+        private static void Welcome5153()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}

7fce2a9869cd13df8bed2aff5afc318750ee2ee1
נועה טימסיט <ntimsit@g.jct.ac.il>
2020-10-21 20:32:33 +0300

    coding the function welcome4372

---
+++ dotNet5781_00_5153_4372/Program4372.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_5153_4372
+{
+    partial class Program
+    {
+        static partial void Welcome4372()
+        {
+            Console.WriteLine("hi, i am also here");
+        }
+    }
+}

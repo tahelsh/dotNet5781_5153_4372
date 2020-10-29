4ad2b1cc137ad1c0c7b9dfd935528f3d709ef0ae
TahelSh <tahelsharon01@gmail.com>
2020-10-29 22:28:37 +0200

    define Bus class

---
+++ dotNet5781_01_5153_4372/Bus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_5153_4372
+{
+    class Bus
+    {
+        public DateTime dateStart { get; set; } //date of start operation
+       
+        private int totalKm;  //Total Kilometrage
+
+        public int TotalKm
+        {
+            get { return totalKm; }
+            set {if (totalKm < value)
+                    totalKm = value;
+                else
+                    throw new Exception("The value for total KM is not valid");// "Error Total Kilometerage"
+
+                 }
+        }
+        public int fuel { get; set; } //Fuel
+
+        private string lisNum; //liscense number
+
+        public string LisNum
+        {
+            get { return lisNum;}
+            set {
+                int a;
+                bool b = int.TryParse(value, out a);
+                if(b)
+                {
+                    if (value.Length == 7 && dateStart.Year < 2018 || value.Length == 8 && dateStart.Year >= 2018)
+                        lisNum = value;
+
+                }
+                else
+                    throw new Exception("The liscense number / the year is not valid");
+
+            }
+        }
+
+    }
+   // public bool Check()
+   // {
+
+   // }
+}

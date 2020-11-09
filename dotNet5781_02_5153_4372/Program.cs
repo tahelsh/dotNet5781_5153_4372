cca0e53748bf94b3099178e111c23e9141d5a5ee
נועה טימסיט <ntimsit@g.jct.ac.il>
2020-11-09 11:07:31 +0200

    defining station class. 9.11

---
+++ dotNet5781_02_5153_4372/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_5153_4372
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

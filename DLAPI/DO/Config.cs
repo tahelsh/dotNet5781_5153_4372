200a03e4324385e157bce2c23f64e1e655ae48b9
נועה טימסיט <ntimsit@g.jct.ac.il>
2020-12-31 21:31:57 +0200

    31/12

---
+++ DLAPI/DO/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    static class Config
+    {
+        //for running numbers
+        public static int LineID = 0;
+        //public static int StationCode = 0;
+        public static int TripId = 0;
+        public static int LineTripId = 0;
+
+
+        //for consts
+        //public static int MAX_LICENSE_NUM = 8;
+        //...
+    }
+}
